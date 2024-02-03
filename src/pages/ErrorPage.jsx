f8ee01855466a69cf21bf3c0de0eeddc77a87ebe
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-02-03 13:24:11 +0530

    errorpage is created in ErrorPage.jsx

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
-    <div>
-      erroepage
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-red-500 mb-4">Error 404</h1>
+        <p className="text-lg text-gray-700 mb-8">Page not found</p>
+        <Link to="/">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline-blue active:bg-blue-800">
+            Go to Home
+          </button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
